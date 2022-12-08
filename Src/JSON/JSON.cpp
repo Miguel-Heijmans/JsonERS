59b1c4877e31753d63b43ca6c82f7f332dba2b6e
Miguel-Heijmans <miguelafonso9@gmail.com>
2022-12-08 14:59:33 +0100

    first commit

---
+++ Src/JSON/JSON.cpp
@@ -0,0 +1,93 @@
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+
+
+
+class JSONContext {
+public:
+	std::vector<nlohmann::json> jsonVector;
+
+};
+
+#define Script4DContextClass JSONContext
+#define ERS_API_CREATORNAME "JSON Library"
+#define ERS_API_ENABLE
+#define ERS_API_4DSCRIPT_LOGGING
+#include "4DScriptLibrary/4DScriptLibrary.h"
+
+constexpr const char* Category = "Miguel";
+
+ERS_API void* GetJsonString(const char* inputstring, JSONContext* stateInstance)
+{
+	auto filepath = std::filesystem::current_path().string() + "\\" + std::string(inputstring);//sets the path to the json file
+
+	std::ifstream file_input(filepath);//reads the contents of the json file
+
+	nlohmann::json data = nlohmann::json::parse(file_input); //adds the fetched "input.json" to the local variable "data
+
+	stateInstance->jsonVector.push_back(data);
+
+	
+
+
+
+	return &stateInstance->jsonVector.at(stateInstance->jsonVector.size() - 1); //returns the amount of different groups in the "input.json" file
+}
+
+
+
+ERS_API_DEFINE4DSFUNCTIONLINK(
+	GetJsonString,
+	GetJsonString,
+	Category, "retrieves json string"
+);
+
+
+
+
+ERS_API char* WriteJsonString(const char* inputstring, JSONContext* stateInstance)
+{
+	std::ofstream o("pretty.json");
+	o << std::setw(4) << inputstring << std::endl; //prettyfies and writes the contents of "input.json" to a new file named "pretty.json"
+
+	return CopyString((inputstring)); //returns the amount of different groups in the "input.json" file
+
+
+}
+
+ERS_API_DEFINE4DSFUNCTIONLINK(
+	WriteJsonString,
+	WriteJsonString,
+	Category, "writes json string"
+);
+
+
+ERS_API char* ValuePickerString(void* jsonPointer, const char* inputString, JSONContext* stateInstance) 
+{
+	nlohmann::json* jsonFile = (nlohmann::json*)jsonPointer;
+
+
+	
+
+	for (auto& element : jsonFile->items())
+	{
+		std::cout << element.key() << " " << element.value() << "\n";
+	}
+
+	
+
+	return CopyString("hello");
+
+}
+
+ERS_API_DEFINE4DSFUNCTIONLINK(
+	ValuePickerString,
+	ValuePickerString,
+	Category, "picks value"
+);
+
+
+
---
+++ Tests/JSON_TEST/tests.cpp
@@ -0,0 +1,37 @@
+#include <catch2/catch_all.hpp>
+
+#include "4DSTests/Utility/Utility.h"
+
+
+#include <memory>
+#include <iostream>
+
+
+
+TEST_CASE("Loading ODBC Manager") {
+    std::unique_ptr<Ers::Script4D::Tests::Script4DLibrary> ODBCLibrary;
+    auto executionType = ERSLIBRARYSECTIONTYPE;
+
+    DYNAMIC_SECTION("Create new ODBC Document") {
+        ODBCLibrary.reset();
+        ODBCLibrary = std::make_unique<Ers::Script4D::Tests::Script4DLibrary>("JSON");
+
+
+        void* output = ODBCLibrary->ExecuteFunction< void* (const char*, void*)>(executionType, "GetJsonString", "GetJsonString", "input.json").Get();
+        std::cout << output << "\n";
+
+        void* output2 = ODBCLibrary->ExecuteFunction< void* (const char*, void*)>(executionType, "GetJsonString", "GetJsonString", "jan.json").Get();
+        std::cout << output2 << "\n";
+
+
+        char* output3 = ODBCLibrary->ExecuteFunction< char* (void*, const char*, void*)>(executionType, "ValuePickerString", "ValuePickerString", output2, "joost").Get();
+        std::cout << output3 << "\n";
+
+        //char* output4 = ODBCLibrary->ExecuteFunction< char* (void*, const char*, void*)>(executionType, "ValuePickerString", "ValuePickerString", output, "markers").Get();
+        //std::cout << output4 << "\n";
+
+
+        
+    }
+}
+
